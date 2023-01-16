4e29acbbcf89ba313360954652dde2a7f9eb2dfe
Ryzon <imtheryzon@gmail.com>
2023-01-16 14:13:56 -0500

    Update EasyAI.java

--- EasyAI.java
+++ EasyAI.java
@@ -1,7 +1,4 @@
 /**
- * @author Mark Wang
- * 1/14/2023
- *
  * This class will implement the simple AI that will use a randomized
  * attack strategy when incorporated.
  */
@@ -35,7 +32,7 @@ public class EasyAI {
         // set values for vector
         vector = new int[][] {
                 {1, -1, 0, 0}, // vector for moving vertically (row vector)
-                {0, 0, 1, -1} // vector for moving horizontally column vector)
+                {0, 0, 1, -1} // vector for moving horizontally (column vector)
         };
 
         // set values for countHit
@@ -58,22 +55,33 @@ public class EasyAI {
      */
     public void placeShips() {
         int ID = 1; // current ID of the ship
-        // for loop to place all ships from destroyer (2 holes) to carrier (5 holes)
+        // for loop to place all ships based on their length
+        // Destroyer: 2 holes
+        // Submarine: 3 holes
+        // Cruiser: 3 holes
+        // Battleship: 4 holes
+        // Carrier: 5 holes
         for (int length : new int[]{2, 3, 3, 4, 5}) {
             while (true) {
                 int rowPlace = random.nextInt(10) + 1; // get a random integer in the range [1, 10]
                 int colPlace = random.nextInt(10) + 1; // get a random integer in the range [1, 10]
                 int idx = random.nextInt(4); // get a random integer in the range [0, 3]
-                boolean works = true;
-                for (int i = 1, curRow = rowPlace, curCol = colPlace; i <= length; i++) {
-                    if (!isValid(curRow, curCol)) {
+                boolean works = true; // boolean that tracks whether the current configuration works or not
+                int curRow = rowPlace; // set the current row to the row set by Random
+                int curCol = colPlace; // set the current column to the column set by Random
+                // Create a for loop that checks if the current position of the ship is valid or not
+                for (int i = 1; i <= length; i++) {
+                    // If it is out of bounds or there is already a ship on the current square, it is invalid
+                    if (!isValid(curRow, curCol) || board[curRow][curCol] != 0) {
                         works = false;
                         break;
                     }
+                    // go to the next cell by moving the coordinates via the vector array
                     curRow += vector[0][idx];
                     curCol += vector[1][idx];
                 }
-                if (!works) continue;
+                if (!works) continue; // If it is invalid, then try another coordinate
+                // If everything is valid, then loop again and place the ID on the set squares
                 for (int i = 1; i <= length; i++) {
                     board[rowPlace][colPlace] = ID;
                     rowPlace += vector[0][idx];
@@ -86,6 +94,22 @@ public class EasyAI {
     }
 
     /**
+     * This method will return if the current ship is destroyed given the ID of the ship and
+     * the amount of hits the ship took
+     * @param countHit
+     * @param ID
+     * @return
+     */
+    public boolean destroyedShip(int countHit, int ID) {
+        if (ID == 1 && countHit == 2) return true;
+        if (ID == 2 && countHit == 3) return true;
+        if (ID == 3 && countHit == 3) return true;
+        if (ID == 4 && countHit == 4) return true;
+        if (ID == 5 && countHit == 5) return true;
+        return false;
+    }
+
+    /**
      * This method will be used in the game when the opponent guesses a coordinate, and will inform
      * them if they hit or missed
      * @param coordinate
@@ -93,13 +117,14 @@ public class EasyAI {
      */
     public HitOrMiss hitOrMiss(Coordinate coordinate) {
         if (board[coordinate.getX()][coordinate.getY()] == 0) {
-            // If no ship is hit, inform that nothing is hit
-            return new HitOrMiss(false, -1, -1);
+            // If no ship is hit, inform that nothing is hit and set default values of false and -1
+            return new HitOrMiss(false, -1, -1, false);
         }
+        // If it is a hit, then return the ID of the ship hit and the total hits this ship induced
         int currentID = board[coordinate.getX()][coordinate.getY()];
-        countHit[currentID]++;
-        board[coordinate.getX()][coordinate.getY()] = 0;
-        return new HitOrMiss(true, currentID, countHit[currentID]);
+        countHit[currentID]++; // update countHit
+        board[coordinate.getX()][coordinate.getY()] = 0; // reset the current block to be 0
+        return new HitOrMiss(true, currentID, countHit[currentID], destroyedShip(countHit[currentID], currentID));
     }
 
     /**
@@ -118,8 +143,25 @@ public class EasyAI {
      * However, since this is an Easy AI, it will do nothing about this since it has no strategy
      * based on if it's a hit or miss, so nothing will be implemented here.
      * @param hit
+     * @param destroyed
      */
-    public void getInformation(boolean hit) {
+    public void getInformation(boolean hit, boolean destroyed) {
 
+    }
+
+    /**
+     * Getter for the board, note that it is not necessary to make a setter for this
+     * @return
+     */
+    public int[][] getBoard() {
+        return this.board;
+    }
+
+    /**
+     * Getter for the countHit array, note that it is not necessary to make a setter for this
+     * @return
+     */
+    public int[] getCountHit() {
+        return this.countHit;
     }
 }
