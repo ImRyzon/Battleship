9201171399c80e8787ecf4ae8bbf79fe60470785
Ryzon <imtheryzon@gmail.com>
2023-01-18 14:42:06 -0500

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -1,75 +1,97 @@
-import javax.swing.*;
+/**
+ * @author Mark Wang
+ * 2022-12-13
+ *
+ * This class will display the rules of Break A Plate
+ */
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
-import java.util.Scanner;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Rules implements ActionListener {
+public class Rules extends JFrame implements ActionListener {
 
-    JFrame frame = new JFrame();
+    /**
+     * These are the necessary objects that help make the Rules page function properly
+     * panel --> the container that stores all the needed components
+     * frame --> the container that acts as the actual window of the rules page
+     * title --> The title of this page, which is "Rules"
+     * rules --> the array containing all the rules to be outputted
+     * backToMenu --> the button that redirects the user back to the menu screen
+     * rules --> the image icon used for this page
+     */
+    JPanel panel = new JPanel();
+    JLabel title = new JLabel("Rules");
+    JLabel[] rules = new JLabel[6];
+    JButton backToMenu = new JButton("Menu");
+    ImageIcon rulesIcon = new ImageIcon("rules.png");
 
-    JPanel titlePanel = new JPanel();
+    /**
+     * This constructor will allow other classes to instantiate an object of the menu class
+     * as well as implement the necessary logic to make the menu function as intended
+     */
+    Rules() {
+        // Set the frame size, close operation, visibility, title, background, icon, and add the panel
+        this.setSize(525, 550);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.add(panel);
+        this.setTitle("Rules");
+        this.setBackground(Color.WHITE);
+        this.setIconImage(rulesIcon.getImage());
 
-    JLabel titleText = new JLabel();
+        // Set the mode of the panel to absolute positioning and set the appropriate colors
+        panel.setLayout(null);
+        panel.setBackground(new Color(255, 204, 203));
 
-    JLabel rulesText = new JLabel();
+        // Customize the title text field and add it to the panel
+        title.setBounds(215, 10, 420, 50);
+        title.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+        title.setBorder(BorderFactory.createLineBorder(new Color(255, 204, 203), 0));
+        title.setBackground(new Color(255, 204, 203));
+        title.setVisible(true);
+        panel.add(title);
 
-    JButton menuButton = new JButton("Menu");
+        // Set and customize the rules array, then add it to the panel
+        rules[0] = new JLabel("The objective is to break as many plates as possible\n");
+        rules[1] = new JLabel("1. A random amount of plates will be broken\n");
+        rules[2] = new JLabel("2. If all plates are broken, you win a Tiger Plush\n");
+        rules[3] = new JLabel("3. If two plates are broken, you win a sticker\n");
+        rules[4] = new JLabel("4. If one plate is broken, you win a tattoo\n");
+        rules[5] = new JLabel("5. If none of the plates are broken, you lose\n");
 
-    Rules() throws Exception {
-
-        //setting up and customizing the JFrame object
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(750, 450);
-        frame.setLayout(new BorderLayout());
-        frame.setVisible(true);
-        frame.setTitle("Menu");
-        //frame.setResizable(false);
-
-        //setting up and customizing the playButton object
-        menuButton.setFocusable(false); //set the focusable to false
-        menuButton.setPreferredSize(new Dimension(250, 60)); //set preferred dimensions
-        menuButton.setFont(new Font("MV Boli", Font.BOLD, 40)); //sets the text font and text size
-        menuButton.addActionListener(this); //adding an ActionListener
-        menuButton.setBackground(Color.WHITE); //set the color of the button
-        menuButton.setForeground(Color.BLACK); //set the color of the text
-        menuButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 7)); //sets the border for the button
-
-        titleText.setText("Rules");
-        titleText.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
-        titleText.setForeground(Color.BLACK);
-        titleText.setBounds(0, 0, 100, 50);
-
-        rulesText.setText("1. Placeholder\n2. Placeholder\n3. Placeholder");
-        rulesText.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
-        rulesText.setForeground(Color.BLACK);
-        rulesText.setBounds(0, 0, 100, 20);
-
-        //changing the color of the 2 panel
-        titlePanel.setBackground(Color.GRAY);
-
-        //adding the components to the titlePanel object
-        titlePanel.add(titleText);
-
-        //adding the panels to the frame
-        frame.add(titlePanel, BorderLayout.NORTH);
-        frame.add(rulesText, BorderLayout.CENTER);
-        frame.add(menuButton, BorderLayout.SOUTH);
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
-        //if menuButton is clicked
-        if(e.getSource() == menuButton) {
-            frame.dispose(); //delete the current frame
-
-            //use try and catch to instantiate an object for Game class
-            try {
-                Menu menu = new Menu();
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
+        int y = 95;
+        for (int i = 0; i < 6; i++, y += 50) {
+            rules[i].setBounds(50, y, 450, 55);
+            rules[i].setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+            rules[i].setBorder(BorderFactory.createLineBorder(new Color(255, 204, 203), 0));
+            rules[i].setBackground(new Color(255, 204, 203));
+            rules[i].setVisible(true);
+            panel.add(rules[i]);
         }
-    }    
+
+        // Customize the menu button and add it to the panel
+        backToMenu.setText("Menu");
+        backToMenu.setBounds(195, 425, 100, 50);
+        backToMenu.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+        backToMenu.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        backToMenu.setBackground(Color.WHITE);
+        backToMenu.addActionListener(this);
+        panel.add(backToMenu);
+
+        // Make the panel visible to the user
+        panel.setVisible(true);
+    }
+
+    /**
+     * This method will take the user back to the menu after they pressed the menu button
+     * @param event
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == backToMenu) { // bring user back to menu if they wish
+            this.dispose();
+            Menu menu = new Menu();
+        }
+    }
 }
