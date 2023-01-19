82ae298103e46b94b086f07c478cdca226d1ed92
Ryzon <imtheryzon@gmail.com>
2023-01-19 18:57:55 -0500

    Update HitOrMiss.java

--- HitOrMiss.java
+++ HitOrMiss.java
@@ -1,4 +1,7 @@
 /**
+ * @author Mark Wang
+ * 2023-1-17
+ *
  * This class will use a custom object that is structured to give information on a guessed
  * coordinate. It will inform if it is a hit or miss, what ship ID is hit (if any ship is hit), and
  * how many hits in total this ship has been hit including the current hit (if any ship is hit
