7872c96f354ad9690d4498971b75b08b4b3f3eaf
Ryzon <imtheryzon@gmail.com>
2023-01-18 21:34:06 -0500

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -1,13 +1,15 @@
 /**
  * @author Mark Wang
- * 2022-12-13
+ * 2023-1-18
  *
- * This class will display the rules of Break A Plate
+ * This class will display the rules of Battleship
  */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
+import java.io.*;
 
 public class Rules extends JFrame implements ActionListener {
 
@@ -18,21 +20,23 @@ public class Rules extends JFrame implements ActionListener {
      * title --> The title of this page, which is "Rules"
      * rules --> the array containing all the rules to be outputted
      * backToMenu --> the button that redirects the user back to the menu screen
-     * rules --> the image icon used for this page
+     * rulesIcon --> icon for the current page
+     * rulesFile --> file to store all the rules
      */
     JPanel panel = new JPanel();
     JLabel title = new JLabel("Rules");
-    JLabel[] rules = new JLabel[6];
+    JLabel[] rules = new JLabel[20];
     JButton backToMenu = new JButton("Menu");
     ImageIcon rulesIcon = new ImageIcon("rules.png");
+    File rulesFile = new File("Rules.txt");
 
     /**
      * This constructor will allow other classes to instantiate an object of the menu class
      * as well as implement the necessary logic to make the menu function as intended
      */
-    Rules() {
+    Rules() throws Exception {
         // Set the frame size, close operation, visibility, title, background, icon, and add the panel
-        this.setSize(525, 550);
+        this.setSize(960, 1265);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.add(panel);
@@ -42,38 +46,34 @@ public class Rules extends JFrame implements ActionListener {
 
         // Set the mode of the panel to absolute positioning and set the appropriate colors
         panel.setLayout(null);
-        panel.setBackground(new Color(255, 204, 203));
+        panel.setBackground(new Color(173, 216, 230));
 
         // Customize the title text field and add it to the panel
-        title.setBounds(215, 10, 420, 50);
-        title.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+        title.setBounds(400, 10, 420, 50);
+        title.setFont(new Font("Comic Sans MS", Font.BOLD, 50));
         title.setBorder(BorderFactory.createLineBorder(new Color(255, 204, 203), 0));
         title.setBackground(new Color(255, 204, 203));
         title.setVisible(true);
         panel.add(title);
 
-        // Set and customize the rules array, then add it to the panel
-        rules[0] = new JLabel("The objective is to break as many plates as possible\n");
-        rules[1] = new JLabel("1. A random amount of plates will be broken\n");
-        rules[2] = new JLabel("2. If all plates are broken, you win a Tiger Plush\n");
-        rules[3] = new JLabel("3. If two plates are broken, you win a sticker\n");
-        rules[4] = new JLabel("4. If one plate is broken, you win a tattoo\n");
-        rules[5] = new JLabel("5. If none of the plates are broken, you lose\n");
+        getRules(); // get all the rules
 
+        // Loop though the positions of all the rules
         int y = 95;
-        for (int i = 0; i < 6; i++, y += 50) {
-            rules[i].setBounds(50, y, 450, 55);
-            rules[i].setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        for (int i = 0; i < rules.length; i++) {
+            rules[i].setBounds(65, y, 900, 55);
+            rules[i].setFont(new Font("Comic Sans MS", Font.BOLD, 17));
             rules[i].setBorder(BorderFactory.createLineBorder(new Color(255, 204, 203), 0));
             rules[i].setBackground(new Color(255, 204, 203));
             rules[i].setVisible(true);
             panel.add(rules[i]);
+            y += 50;
         }
 
         // Customize the menu button and add it to the panel
         backToMenu.setText("Menu");
-        backToMenu.setBounds(195, 425, 100, 50);
-        backToMenu.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+        backToMenu.setBounds(405, 1125, 125, 75);
+        backToMenu.setFont(new Font("Monospaced", Font.BOLD, 30));
         backToMenu.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
         backToMenu.setBackground(Color.WHITE);
         backToMenu.addActionListener(this);
@@ -81,6 +81,24 @@ public class Rules extends JFrame implements ActionListener {
 
         // Make the panel visible to the user
         panel.setVisible(true);
+    }
+
+    /**
+     * this method will get all the rules from the rules file and store them into the
+     * JLabel array to be outputted on the screen
+     * @throws Exception
+     */
+    void getRules() throws Exception {
+        Scanner readRules = new Scanner(rulesFile); // create scanner to read rules
+
+        // get all the rules from the file via scanner
+        int index = 0;
+        while (readRules.hasNextLine()) {
+            rules[index] = new JLabel(readRules.nextLine());
+            index++;
+        }
+
+        readRules.close(); // close scanner
     }
 
     /**
