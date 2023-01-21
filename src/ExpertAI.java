bef762200153aecdc4daf84fe572f1030d3aaca7
Ryzon <imtheryzon@gmail.com>
2023-01-21 16:49:28 -0500

    Update ExpertAI.java

--- src/ExpertAI.java
+++ src/ExpertAI.java
@@ -103,7 +103,7 @@ public class ExpertAI {
     }
 
     /**
-     * This method will place thw five ships in valid positions and store their IDs on the board
+     * This method will place the five ships in valid positions and store their IDs on the board
      */
     public void placeShips() {
         int ID = 1; // current ID of the ship
