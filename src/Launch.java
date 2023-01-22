b91ebe8c72c2f224db2fe83678d1ecd0f5cc75e6
jashanu <122736904+jashanu@users.noreply.github.com>
2023-01-21 20:21:25 -0500

    Update Launch.java

--- src/Launch.java
+++ src/Launch.java
@@ -9,7 +9,7 @@
 public class Launch {
 
     /**
-     * This main method will launch the Break A Plate game. However, before
+     * This main method will launch the Battleship game. However, before
      * actually playing the game, the user must first go through the login page and
      * enter valid credentials in order to proceed into the actual game, hence why it calls
      * an object of the LoginPage class
