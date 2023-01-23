7318f3daf67c7d3ddb6cba04b2dde07376ec0201
Ryzon <imtheryzon@gmail.com>
2023-01-23 12:12:40 -0500

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,5 @@
 /**
- * @author Nathanael You
+ * @author Nathanael You, Mark Wang, Daniel Guo
  * 2023-1-23
  *
  * This class will act as the game board when playing the game
@@ -17,12 +17,20 @@ public class GameBoard extends JFrame implements ActionListener {
      * defenseGrid --> grid that displays your own ships (defense grid)
      * defensePanel --> panel that displays the defense grid
      * gameIcon --> the icon for this page
+     * friendlyShipsDestroyed --> counts how many of own ships are destroyed
+     * enemyShipsDestroyed --> counts how many of enemy ships are destroyed
+     * friendlyLabel --> label that displays how many friendly ships have been destroyed
+     * enemyLabel --> label that displays how many enemy ships have been destroyed
      */
-    JButton[][] attackGrid = new JButton[11][11];
-    JPanel attackPanel = new JPanel();
-    JButton[][] defenseGrid = new JButton[11][11];
-    JPanel defensePanel = new JPanel();
-    ImageIcon gameIcon = new ImageIcon("GameIcon.png");
+    private JButton[][] attackGrid = new JButton[11][11];
+    private JPanel attackPanel = new JPanel();
+    private JButton[][] defenseGrid = new JButton[11][11];
+    private JPanel defensePanel = new JPanel();
+    private ImageIcon gameIcon = new ImageIcon("GameIcon.png");
+    private int friendlyShipsDestroyed = 0;
+    private int enemyShipsDestroyed = 0;
+    private JLabel friendlyLabel = new JLabel();
+    private JLabel enemyLabel = new JLabel();
 
     /**
      * This constructor will allow for instantiation of this class while also implementing the necessary
@@ -42,21 +50,29 @@ public class GameBoard extends JFrame implements ActionListener {
                 attackGrid[x][y].addActionListener(this);
                 attackGrid[x][y].setBackground(Color.BLACK);
                 attackPanel.add(attackGrid[x][y]);
+
+                // set numbers and characters as the coordinates
                 if(x == 0){
                     attackGrid[x][y].setEnabled(false);
                     if(y != 0){
                         attackGrid[x][y].setText(String.valueOf(y));
+                        attackGrid[x][y].setFont(new Font("Comic Sans MS", Font.BOLD, 13));
                     }
                 }
+
                 if(y == 0){
                     attackGrid[x][y].setEnabled(false);
+                    if(x != 0){
+                        attackGrid[x][y].setText(String.valueOf((char)(x + 'A' - 1)));
+                        attackGrid[x][y].setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+                    }
                 }
             }
         }
 
         // customize defensePanel
         defensePanel.setLayout(new GridLayout(11, 11, 2, 2));
-        defensePanel.setBounds(700, 10, 300, 300);
+        defensePanel.setBounds(700, 10, 450, 450);
         defensePanel.setBackground(new Color(52, 152, 235));
 
         // create defense grid and customize it
@@ -67,21 +83,29 @@ public class GameBoard extends JFrame implements ActionListener {
                 defenseGrid[x][y].addActionListener(this);
                 defenseGrid[x][y].setBackground(Color.BLACK);
                 defensePanel.add(defenseGrid[x][y]);
+
+                // set numbers and letters for the coordinates
                 if(x == 0){
                     defenseGrid[x][y].setEnabled(false);
                     if(y != 0){
                         defenseGrid[x][y].setText(String.valueOf(y));
+                        defenseGrid[x][y].setFont(new Font("Comic Sans MS", Font.BOLD, 5));
                     }
                 }
+
                 if(y == 0){
                     defenseGrid[x][y].setEnabled(false);
+                    if(x != 0) {
+                        defenseGrid[x][y].setText(String.valueOf((char)(x + 'A' - 1)));
+                        defenseGrid[x][y].setFont(new Font("Comic Sans MS", Font.BOLD, 5));
+                    }
                 }
             }
         }
 
         // customize the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1040, 700);
+        this.setSize(1200, 725);
         this.setBackground(Color.WHITE);
         this.setLayout(null);
         this.setTitle("Game Board");
@@ -92,6 +116,14 @@ public class GameBoard extends JFrame implements ActionListener {
         this.add(defensePanel);
         this.setIconImage(gameIcon.getImage()); //Set the game icon image
         this.setVisible(true);
+    }
+
+    /**
+     * This method will update both labels when ships have been destroyed in the game
+     */
+    public void updateLabel() {
+        friendlyLabel.setText("Friendly Ships Destroyed: " + friendlyShipsDestroyed);
+        enemyLabel.setText("");
     }
 
     @Override
