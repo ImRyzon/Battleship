ac661adbd13de2091f566fd6db77d9c06ab4e583
clides <daniel168.guo@gmail.com>
2023-01-23 19:35:41 -0500

    changed stuff

--- src/Rules.java
+++ src/Rules.java
@@ -5,6 +5,13 @@
  * This class will display the rules of Battleship
  */
 
+import java.io.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -102,14 +109,36 @@ public class Rules extends JFrame implements ActionListener {
         readRules.close(); // close scanner
     }
 
+    public void playSound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("buttonsound.wav"));
+        
+        // create clip reference
+        Clip clip = AudioSystem.getClip();
+          
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+
+        clip.start();
+    }
+
     /**
      * This method will take the user back to the menu after they pressed the menu button
      * @param event
      */
     @Override
     public void actionPerformed(ActionEvent event) {
+
         if (event.getSource() == backToMenu) { // bring user back to menu if they wish
+            
+            try {
+                playSound();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
             this.dispose();
+
             Menu menu = new Menu();
         }
     }
