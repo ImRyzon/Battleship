09b85599b49f22997676fe273fc16118af42ffac
Ryzon <imtheryzon@gmail.com>
2023-01-27 13:20:01 -0500

    Update EasyAI.java

--- src/EasyAI.java
+++ src/EasyAI.java
@@ -110,10 +110,13 @@ public class EasyAI {
      * This method will store the board to a file
      */
     public void storeBoard() {
+        // try-amd-catch statement for exceptions
         try {
+            // declare file and PrintWriter object
             File file = new File("AIBoard.txt");
             PrintWriter writer = new PrintWriter(file);
 
+            // print the current board to the file
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
                     writer.print(board[i][j]);
